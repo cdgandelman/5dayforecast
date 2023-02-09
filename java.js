8e715a0fb72f209b344e2696d974a8579b9401c0
cdgandelman <cdgandelman@yahoo.com>
2023-02-09 12:41:24 -0700

    Corrections

--- java.js
+++ java.js
@@ -60,3 +60,18 @@ var getForecast = function (lat, lon) {
     })
 
 }
+//Day 
+var d =new Date();
+var weekday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function CheckDay(day){
+    if(day+d.getDay()>6){
+        return day+d.getDay()-7;
+    }
+    else{
+        return day+d.getDay();
+    }
+}
+for(i=0;i<5;i++){
+    document.getElementById("day"+(i+1)).innerHTML=weekday[CheckDay(i)];
+}
